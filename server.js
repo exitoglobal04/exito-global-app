f026b1625cd119410f04c05cf8e1c0b378bd352b
Exito Global <exitoglobal04@gmail.com>
2025-06-23 10:03:07 -0500

    Añadir ruta de prueba

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const db = new sqlite3.Database('database.db');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/test', (req, res) => {
+  res.send('Servidor funcionando');
+});
+
 // Crear tabla de usuarios si no existe
 db.run(`
   CREATE TABLE IF NOT EXISTS usuarios (
